99682223ff95ef90fd9b7526a2e6dfcf211d7eca
na1-4an <parkne0114@naver.com>
2022-08-17 00:53:55 +0900

    파일 정리

--- README.md
+++
@@ -1,3 +0,0 @@
-## login-lecture
-백엔드 맛보기 강의
-
--- src/bin/www.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const PORT = 3000;
-const app = require("../app")
-
-//listen으로 서버 열기
-app.listen(PORT, () => {
-    console.log("서버 가동");
-});
