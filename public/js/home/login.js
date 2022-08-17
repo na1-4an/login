0b19fa9f47929a2de7b9d72c07c3c2a73408cbae
na1-4an <parkne0114@naver.com>
2022-08-17 12:06:58 +0900

    돔을 이용하여 HTML 객체 저장

--- public/js/home/login.js
+++ public/js/home/login.js
@@ -1,3 +1,17 @@
 "use strict"
 
-console.log("hihi");
+//DOM(document object model)은 일종의 인터페이스로 자바스크립트에서 html으로 받은 데이터를 제어 가능케함.
+const id = document.querySelector("#id"),
+    password = document.querySelector("#password"),
+    loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id: id.value,
+        password: password.value,
+    };
+    console.log(req);
+    
+}
